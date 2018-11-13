4fd24cc9100d1237567df3093f3f1c91a4b26184
Dan Mickulesku <danmickulesku@gmail.com>
2018-11-13 18:29:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function saHiToGrandma(string) {
-  var lowercase === string.toLowerCase
+  var lowercase = 'hello'
   if (string.toLowerCase === lowercase) {
     return "I can\'t hear you!"
   }
